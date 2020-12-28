3132cfc22c2ac658f17ddf688a85567cec5fd5ac
风光霁月 <294039391@qq.com>
2020-12-28 22:26:53 +0800

    Change the cancer_type get method

--- cancerTypeUnit.py
+++ cancerTypeUnit.py
@@ -1,6 +1,8 @@
-def get_cancer(s,study_id):
-    cancer = s.Studies.getStudyUsingGET(
-        studyId = study_id
-    ).result().cancerType.name
+def get_cancer_type(s,study_id,sample_id):
+    cancer_type = s.Clinical_Data.getAllClinicalDataOfSampleInStudyUsingGET(
+        attributeId = 'CANCER_TYPE_DETAILED',
+        sampleId = sample_id,
+        studyId = study_id,
+    ).result()[0].value
 
-    return cancer
+    return cancer_type
